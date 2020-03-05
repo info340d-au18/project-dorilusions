e1bea7234097423633401dc1664fee64ffdc07e8
Aaron Zhao <zhiyuz2@uw.edu>
2020-03-04 17:09:20 -0800

    minor improvement

--- src/App.js
+++ src/App.js
@@ -1,67 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
-import { HashRouter as Router, Route, Link, useRouteMatch } from "react-router-dom";
-import { Homepage } from './Home';
-import { About } from './About';
-import { Engage } from './Engage';
-import { Booking } from './Booking';
-import { Signin } from './Signin';
-import { AdminHome } from './AdminHome';
-
-function AdminHome(){
-  let { path, url } = useRouteMatch();
-
-  return (
-    <div>
-      <h1>Welcome Back!</h1>
-      <Link to={`${url}/memberdata`}>Member Data</Link>
-
-      <Route path={`${path}/`} component={Topic}/>
-    </div>
-  )
-}
-=======
 import { NavBar } from './navBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 823d73264b5e3a06b601788393ba3eb91466bf64
 
 class App extends Component {
     render() {
         return (
           < NavBar ></NavBar>
-          
-            // <Router>
-
-<<<<<<< HEAD
-                <Route exact path="/" component={ Homepage } />
-                <Route path="/about" component={ About } />
-                <Route path="/engage" component={ Engage } />
-                <Route path="/booking" component={ Booking } />
-                <Route path="/signin">
-                  <Signin/>
-                </Route>/>
-              </div>
-            </Router>
-=======
-            
-
-              
-                // <Link to="/">Home</Link>
-                // <Link to="/about">About</Link>
-                // <Link to="/engage">Engage</Link>
-                // <Link to="/booking">Booking</Link>
-                // <Link to="/signin">Sign In</Link>
-
-            //     <Route exact path="/" component={ Homepage } />
-            //     <Route path="/about" component={ About } />
-            //     <Route path="/engage" component={ Engage } />
-            //     <Route path="/booking" component={ Booking } />
-            //     <Route path="/signin" component={ Signin } />
-           
-              
-            // </Router>
->>>>>>> 823d73264b5e3a06b601788393ba3eb91466bf64
             );
     }
 }
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import bg from './img/minidoka.jpg'
+import './App.css';
+import './w3.css'
 
 export class Homepage extends Component {
     render() {
-        return <div>Home page</div>
+        return (
+            <div class="w3-content">
+                <div class="w3-display-container w3-center">
+                    <img class="banner" src={bg} alt="Minidoka 2017"></img>
+                    <div class="w3-display-middle w3-container w3-text-white w3-padding-64 w3-hide-small">
+                        <h1 class="w3-text-white">Sunday, Feburary 16</h1>
+                        <h1 class="w3-jumbo w3-text-white w3-wide w3-margin-bottom">Minidoka Day of Rememberance</h1>    
+                        <h6><a href="#upcomingPerformances"><button
+                            class="w3-button w3-white w3-padding-large w3-large w3-opacity w3-margin-top w3-hover-opacity-off">MORE
+                            UPCOMING EVENTS</button></a></h6>
+                    </div>
+                </div>
+            </div>
+
+        )
     }
-}
+} 
