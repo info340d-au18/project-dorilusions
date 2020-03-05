823d73264b5e3a06b601788393ba3eb91466bf64
Aaron Zhao <zhiyuz2@uw.edu>
2020-03-04 16:18:25 -0800

    Changes basic setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Link } from "react-router-dom";
-import { Homepage } from './Home';
-import { About } from './About';
-import { Engage } from './Engage';
-import { Booking } from './Booking';
-import { Signin } from './Signin';
+import { NavBar } from './navBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render() {
         return (
-            <Router>
-              <div>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/engage">Engage</Link>
-                <Link to="/booking">Booking</Link>
-                <Link to="/signin">Sign In</Link>
+          < NavBar ></NavBar>
+          
+            // <Router>
 
-                <Route exact path="/" component={ Homepage } />
-                <Route path="/about" component={ About } />
-                <Route path="/engage" component={ Engage } />
-                <Route path="/booking" component={ Booking } />
-                <Route path="/signin" component={ Signin } />
-              </div>
-            </Router>
+            
+
+              
+                // <Link to="/">Home</Link>
+                // <Link to="/about">About</Link>
+                // <Link to="/engage">Engage</Link>
+                // <Link to="/booking">Booking</Link>
+                // <Link to="/signin">Sign In</Link>
+
+            //     <Route exact path="/" component={ Homepage } />
+            //     <Route path="/about" component={ About } />
+            //     <Route path="/engage" component={ Engage } />
+            //     <Route path="/booking" component={ Booking } />
+            //     <Route path="/signin" component={ Signin } />
+           
+              
+            // </Router>
             );
     }
 }
