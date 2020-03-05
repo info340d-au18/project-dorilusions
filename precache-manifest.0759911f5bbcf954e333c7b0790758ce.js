5073e5b820d904a462f37284584cb2922dc543d4
dorilusions <dorothyz.lu@gmail.com>
2020-03-04 21:26:04 -0800

    Updates

---
+++ precache-manifest.0759911f5bbcf954e333c7b0790758ce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae32666e0842f7853d3d1985a285fdd",
+    "url": "/project-dorilusions/index.html"
+  },
+  {
+    "revision": "7f15094974a1c93c1f7a",
+    "url": "/project-dorilusions/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f25387b1d20a940bc38f",
+    "url": "/project-dorilusions/static/css/main.1f65db59.chunk.css"
+  },
+  {
+    "revision": "7f15094974a1c93c1f7a",
+    "url": "/project-dorilusions/static/js/2.99e052f2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project-dorilusions/static/js/2.99e052f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25387b1d20a940bc38f",
+    "url": "/project-dorilusions/static/js/main.8bdc5e89.chunk.js"
+  },
+  {
+    "revision": "63109e324c2857011f84",
+    "url": "/project-dorilusions/static/js/runtime-main.e1693a52.js"
+  },
+  {
+    "revision": "a38ced5a8b4d2066a79f1e5359ba290b",
+    "url": "/project-dorilusions/static/media/logo-new.a38ced5a.png"
+  },
+  {
+    "revision": "acc121ad79a7ac83fd1fb86b9cc35960",
+    "url": "/project-dorilusions/static/media/minidoka.acc121ad.jpg"
+  },
+  {
+    "revision": "a9e817718ee27a359bad5bb69becd4fb",
+    "url": "/project-dorilusions/static/media/nsudor.a9e81771.jpg"
+  },
+  {
+    "revision": "448c8840dff7e17fb91f35cb8a21d1f5",
+    "url": "/project-dorilusions/static/media/sakuracon2020.448c8840.jpg"
+  },
+  {
+    "revision": "ea291f2b0c7b8f7af220cde08105b4c3",
+    "url": "/project-dorilusions/static/media/taikokaispringconcert.ea291f2b.jpg"
+  },
+  {
+    "revision": "871ac4492af30aea8694cc2f6f6e431b",
+    "url": "/project-dorilusions/static/media/tosatimeless.871ac449.jpg"
+  }
+]);
