f592ee263643c02fd75c73364f9ec33ab3441ca9
Aaron Zhao <zhiyuz2@uw.edu>
2020-03-10 21:04:20 -0700

    Minor fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Homepage } from './Home';
+import { About } from './About';
+import { Engage } from './Engage';
+import { Booking } from './Booking';
+import { Signin } from './Signin';
+import { MemberDataTable } from './datatable';
+import { SongDataTable } from './datatable';
+
+import logo from './img/logo-new.png';
+
 import './w3.css';
+
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+
 import './App.css';
 import { NavBar } from './navBar';
 import { Footer } from './Footer';
@@ -9,7 +23,31 @@ class App extends Component {
     render() {
         return (
           <div>
-            < NavBar></NavBar>
+            <Router>
+                <nav class="w3-top">
+                   <div class="w3-bar w3-card transparentNav">
+                        <a class="navLink w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="navToggle()"
+                            title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+                        <Link to="/"><a class="navLink"><img src={logo} class="homeLogo"></img></a></Link>
+                        {/* <Link to="/engage"><a class="navLink w3-hide-small">ENGAGE</a></Link> */}
+                        <Link to="/booking"><a class="navLink w3-hide-small">BOOKING</a></Link>
+                        <Link to="/about"><a class="navLink w3-hide-small">ABOUT</a></Link>
+                        <Link to="/memberdata"><a class="navLink w3-hide-small">MEMBERS</a></Link>
+                        <Link to="/songdata"><a class="navLink w3-hide-small">SONGS</a></Link>
+                        <Link to="/signin"><a class="navLink signIn w3-hide-small">SIGN IN</a></Link>
+                    </div>
+                </nav>
+
+                <Route exact path="/" component={ Homepage } />
+                <Route path="/about" component={ About } />
+                {/* <Route path="/engage" component={ Engage } /> */}
+                <Route path="/booking" component={ Booking } />
+                <Route path="/signin" component={ Signin } />
+                <Route path="/memberdata" component={ MemberDataTable } />
+                <Route path="/songdata" component={ SongDataTable } />
+
+            </Router>
+            
           </div>
         );
     }
